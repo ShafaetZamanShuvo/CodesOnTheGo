397edd662c20b2ad10fa02b6cd13efd032c6a668
ShafaetZamanShuvo <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2022-07-28 18:04:00 +0600

    I tried coding on ipad but couldn't run it :(

---
+++ myFirstCode.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    cout << "Give input for a and b\n";
+    cin >> a >> b;
+    c = a+b;
+    cout << "Sum = " << c <<"\n";
+
+}
