28ea6a671924b4116f6b36b9c31ee3342ad1ee1b
ShafaetZamanShuvo <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2022-08-08 11:59:04 +0600

    array concat

---
+++ ConcatenationOfArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector <int> ans;
+        for(int j = 0; j<2; j++)
+        {
+
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans.push_back(nums[i]);
+        }
+        
+    }
+
+
+int main() {
+  
+}
