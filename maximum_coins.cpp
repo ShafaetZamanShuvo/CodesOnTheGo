cc3865448472791192c732e94382394756ab2dbb
ShafaetZamanShuvo <1705086@ugrad.cse.buet.ac.bd>
2022-08-20 17:05:41 +0000

    Commit from my ipad

---
+++ maximum_coins.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Give input for a b and c:" << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout << a+b+c << endl;
+    cout << "Hello World" << endl;
+}
