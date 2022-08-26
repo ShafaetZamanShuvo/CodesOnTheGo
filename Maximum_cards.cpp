650a5a5f13d358d3d39255bf3e73c3fe72da9727
ShafaetZamanShuvo <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2022-08-26 02:13:14 +0000

    TLE On maximum_cards.cpp

--- Maximum_cards.cpp
+++ Maximum_cards.cpp
@@ -1,63 +1,33 @@
-#include<bits/stdc++.h>
-using namespace std;
-int maxScore(vector<int>& cardPoints, int k){
-    int count = 0;
+class Solution {
+public:
+int maxScore(vector<int> &cardPoints, int k)
+{
+    int count = k;
     int max_score = 0;
     int size = cardPoints.size();
-    for(int i = 0; count<k; )
+    for (int i = 0; i <= count; i++)
     {
-        if(count < k)
-        {
-        if(cardPoints[i] == cardPoints[size - 1 -i])
-        {
-            max_score += cardPoints[i];
-            cout <<endl<< 1 << " "<< cardPoints[i] << endl;
-            cardPoints.erase(cardPoints.begin()+size -i -1);
-            size--;
-            count++;
-            for(int j = 0; j< cardPoints.size(); j++)
-            {
-              cout << cardPoints[j] << " ";
-            }
-            
-        }
-         else if(cardPoints[i]> cardPoints[size - 1 - i])
-        {
-            max_score+= cardPoints[i];
-            cout <<endl<< 2 << " "<< cardPoints[i] << endl;
-            cardPoints.erase(cardPoints.begin()+i);
-            size--;
-            count++;
-            for(int j = 0; j<cardPoints.size(); j++)
-            {
-                cout << cardPoints[j] << " ";
-            }
-            
-        }
-        else if(cardPoints[i] < cardPoints[size - 1 - i])
-        {
-            max_score += cardPoints[size - 1 - i];
-            cout <<endl<< 3 << " "<< cardPoints[size - 1 - i] << endl;
-            cardPoints.erase(cardPoints.begin()+(size - 1 - i));
-            size--;
-            count++;
-            for(int j = 0; j< cardPoints.size(); j++)
-            {
-                cout << cardPoints[j] << " ";
-            }
-            
-        }
-        }
+        int curr = i;
+        int remaining = count - curr;
 
+        int sum = 0;
+
+        for (int j = 0; j < curr; j++)
+        {
+            sum += cardPoints[j];
+        }
+        for (int l = size - 1; l > size - remaining - 1; l--)
+        {
+            sum += cardPoints[l];
+        }
+    
+
+        if (sum > max_score)
+        {
+            max_score = sum;
+        }
     }
 
     return max_score;
-    
 }
-int main()
-{
-    vector<int> cardPoints = {11,49,100,20,86,29,72};
-    int k = 4;
-    cout <<maxScore(cardPoints,k)<<endl; 
-    
-}
+};
