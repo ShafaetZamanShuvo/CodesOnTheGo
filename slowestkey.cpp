3df34b03c5038521fc59c41938b3241c3a4971aa
ShafaetZamanShuvo <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2022-09-02 12:25:16 +0000

    Acpet

--- slowestkey.cpp
+++ slowestkey.cpp
@@ -10,57 +10,46 @@ public:
         int n = releaseTimes.size();
         int distance = 0;
         int max_distance = 0;
-        int max_distance_index = 0;
+        char temp;
+        char slowest;
 
-        vector<char> key_pressed;
-
-        for (int i = 0; i < n - 1; i++)
+        for (int i = 0; i < n; i++)
         {
             if (i == 0)
             {
                 distance = releaseTimes[i];
                 if (distance > max_distance)
+                {
                     max_distance = distance;
-                cout << "max_distance" << max_distance << endl;
-                max_distance_index = i;
+                    slowest = keysPressed[i];
+                }
             }
             if (i > 0)
             {
-                distance = releaseTimes[i] - releaseTimes[i-1];
-                if(distance == max_distance)
+                distance = releaseTimes[i] - releaseTimes[i - 1];
+                if (distance == max_distance)
                 {
-                    key_pressed.push_back(keysPressed[i]);
+                    temp = keysPressed[i];
+                    if (temp > slowest)
+                        slowest = temp;
                 }
-                else if(distance > max_distance)
+                else if (distance > max_distance)
                 {
                     max_distance = distance;
-                    max_distance_index = i;
-                    key_pressed.clear();
-                    key_pressed.push_back(keysPressed[i]);
+                    slowest = keysPressed[i];
                 }
-                
             }
         }
-        for(int i = 0; i< key_pressed.size(); i++)
-        {
-            cout << key_pressed[i] << " "<<endl;
-        }
 
-        //sort the key_pressed
-        sort(key_pressed.begin(), key_pressed.end());
-
-        // cout << "The answer is " << key_pressed[0] << endl;
-
-        return key_pressed[key_pressed.size()-1];; 
-
+        return slowest;
     }
 };
 
 int main()
 {
     Solution s;
-    vector<int> releaseTimes = {12,23,36,46,62};
-    string keysPressed = "spuda";
+    vector<int> releaseTimes = {28, 65, 97};
+    string keysPressed = "gaf";
     cout << s.slowestKey(releaseTimes, keysPressed) << endl;
     return 0;
 }
