03938f22396f7104a0d28c8e5328d70052e6964c
Md. Shafaet Zaman <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2023-02-17 12:08:39 +0600

    Add two numbers via linklist

---
+++ leetcode2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+
+       ListNode *head = new ListNode();
+         ListNode *temp = head;
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL || carry)
+        {
+            int sum = 0;
+            if (l1 != NULL)
+            {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL)
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            temp->next = new ListNode(sum);
+            temp = temp->next;
+        }
+        return head->next;
+    }
+};
+
+int main()
+{
+    ListNode *l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    ListNode *l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    Solution s;
+    ListNode *head = s.addTwoNumbers(l1, l2);
+
+    cout << "after adding the two numbers" << endl;
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    return 0;
+}
