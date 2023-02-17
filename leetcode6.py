c27a1010563a2b21f7d43a2e38d1922932196dc3
Md. Shafaet Zaman <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2023-02-17 19:51:20 +0600

    ZigZag a string

---
+++ leetcode6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if (numRows == 1):
+            return str
+        ans = ""
+        for r in range(numRows):
+            increment = (numRows-1)*2
+            for i in range(r,len(s), increment):
+                ans+= s[i]
+                if(r>0 and r<numRows-1 and i+increment-2*r < len(s)):
+                    increment = increment-2*r
+                    ans+= s[i+increment]
+
+        return ans            
