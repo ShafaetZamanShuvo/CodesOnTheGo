df0bcaa05ccf6a3a357fc65c36f6f43659c21128
Md. Shafaet Zaman <75299063+ShafaetZamanShuvo@users.noreply.github.com>
2023-02-19 01:02:48 +0600

    LCS

---
+++ leetcode1143.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+
+        int m = text1.size();
+        int n = text2.size();
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+        //bottom up approach
+
+        for(int i = m-1; i >= 0; i--)
+        {
+            for(int j = n-1; j >= 0; j--)
+            {
+                if(text1[i] == text2[j])
+                {
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+           
+        }
+
+        return dp[0][0];
+        
+
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.longestCommonSubsequence("abcde", "ace") << endl;
+
+}
